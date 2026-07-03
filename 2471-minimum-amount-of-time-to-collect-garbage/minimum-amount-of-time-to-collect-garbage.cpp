6e85dc343903f85eadec314feaf048484a7b23e0
ironarc <168215239+amankagithubhehai@users.noreply.github.com>
2026-07-03 20:57:30 +0530

    Time: 24 ms (30.19%) | Memory: 107 MB (89.63%) - LeetSync

---
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int cnt=0;
+        int p=0,m=0,g=0,a=0;
+        for(int i=0;i<garbage.size();i++){
+            cnt+=garbage[i].size();
+            for(int j=0;j<garbage[i].size();j++){
+               
+               if(garbage[i][j]=='G')  g=max(g,i);
+               else if(garbage[i][j]=='M')  m=max(m,i);
+               else if(garbage[i][j]=='P')  p=max(p,i);
+            }
+            a=max({p,m,g});
+        }
+      
+      for (int i = 0; i < travel.size(); i++) {
+    if (i < p) cnt += travel[i];
+    if (i < m) cnt += travel[i];
+    if (i < g) cnt += travel[i];
+}
+      return cnt;
+    }
+};
