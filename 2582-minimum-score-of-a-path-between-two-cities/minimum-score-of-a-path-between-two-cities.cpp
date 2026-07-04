2b0c38a0ab69353efd2ec260a459869720c451b1
ironarc <168215239+amankagithubhehai@users.noreply.github.com>
2026-07-04 11:22:38 +0530

    Time: 12 ms (94.90%) | Memory: 106.6 MB (99.05%) - LeetSync

---
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> parent;
+
+    int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void unite(int a, int b) {
+        parent[find(a)] = find(b);
+    }
+
+    int minScore(int n, vector<vector<int>>& roads) {
+        parent.resize(n + 1);
+
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (auto &r : roads) {
+            unite(r[0], r[1]);
+        }
+
+        int root = find(1);
+        int ans = INT_MAX;
+
+        for (auto &r : roads) {
+            if (find(r[0]) == root) {
+                ans = min(ans, r[2]);
+            }
+        }
+
+        return ans;
+    }
+};
